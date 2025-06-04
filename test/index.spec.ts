e48792f95e5c350684e82196f579276ffe8d1bfa
Alejandro Pato <dev.patoale@gmail.com>
2025-06-04 14:27:39 +0200

    test: add dummy successful test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,4 +2,8 @@ describe('Hello word', () => {
     it('should pass', () => {
         expect(false).toBe(false);
     })
+
+    it('should fail', () => {
+        expect(true).toBe(false);
+    })
 })
